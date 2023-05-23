3d0e1a32b5ac05d9a158931b94cfeaa977b27308
larissax <larissa.christ1@hs-augsburg.de>
2023-05-23 22:37:15 +0200

    playing the game

--- example-app/src/App.js
+++ example-app/src/App.js
@@ -1,38 +1,44 @@
-import './App.css';
-import React, { useEffect, useState } from 'react';
-import web3 from 'web3';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import web3 from "web3";
 import { ethers } from "ethers";
+import { Alert, Button, Placeholder } from "react-bootstrap";
+import { gameContractAbi, gameContractAddress } from "./gameContractInfo";
 
 function App() {
-
   const [account, setAccount] = useState();
+  const [loading, setLoading] = useState(false);
+  const [smartContract, setSmartContract] = useState();
+  const [participated, setParticipated] = useState([]);
 
   // Function to connect wallet
   const connectWallet = async () => {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
     if (accounts.length > 0) {
       const chainId = "1287"; // Moonbase Alpha chainId
-  
+
       if (window.ethereum.networkVersion !== chainId) {
         try {
           await window.ethereum.request({
-            method: 'wallet_switchEthereumChain',
-            params: [{ chainId: web3.utils.toHex(chainId) }]
+            method: "wallet_switchEthereumChain",
+            params: [{ chainId: web3.utils.toHex(chainId) }],
           });
         } catch (err) {
           // This error code indicates that the chain has not been added to MetaMask
           if (err.code === 4902) {
             await window.ethereum.request({
-              method: 'wallet_addEthereumChain',
+              method: "wallet_addEthereumChain",
               params: [
                 {
-                  chainName: 'Moonbase Alpha',
+                  chainName: "Moonbase Alpha",
                   chainId: web3.utils.toHex(chainId),
-                  nativeCurrency: { name: 'DEV', decimals: 18, symbol: 'DEV' },
-                  rpcUrls: ['https://rpc.testnet.moonbeam.network']
-                }
-              ]
+                  nativeCurrency: { name: "DEV", decimals: 18, symbol: "DEV" },
+                  rpcUrls: ["https://rpc.testnet.moonbeam.network"],
+                },
+              ],
             });
           }
         }
@@ -41,47 +47,105 @@ function App() {
       console.log(accounts);
       setAccount(accounts[0]);
     }
-  }
+  };
 
   const getSigner = async () => {
-      // A Web3Provider wraps a standard Web3 provider, which is
-      // what MetaMask injects as window.ethereum into each page
-      const provider = await getProvider();
+    // A Web3Provider wraps a standard Web3 provider, which is
+    // what MetaMask injects as window.ethereum into each page
+    const provider = await getProvider();
 
-      // MetaMask requires requesting permission to connect users accounts
-      await provider.send("eth_requestAccounts", []);
+    // MetaMask requires requesting permission to connect users accounts
+    await provider.send("eth_requestAccounts", []);
 
-      // The MetaMask plugin also allows signing transactions
-      // For this, you need the account signer...
-      const signer = provider.getSigner();
-      return signer;
-  }
+    // The MetaMask plugin also allows signing transactions
+    // For this, you need the account signer...
+    const signer = provider.getSigner();
+    return signer;
+  };
 
   const getProvider = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
     const block = await provider.getBlockNumber();
     console.log(block);
     return provider;
-  }
+  };
 
   // Function to start playing the game
+  const playGame = async (e) => {
+    setLoading(true);
+    const signer = await getSigner();
+    const smartContract = new ethers.Contract(
+      gameContractAddress,
+      gameContractAbi,
+      signer
+    );
+    console.log("Play the game");
+    setSmartContract(smartContract);
 
-  // Function to start listening to events
+    await smartContract.playGame();
+  };
+
+  // Function to start listening to events#
+  useEffect(() => {
+    if (smartContract) {
+      smartContract.on(
+        "Participated",
+        (lastParticipant, lastNumber, lastRandom, winner) => {
+          console.log(lastParticipant, lastNumber, lastRandom, winner);
+          setParticipated((currentParticipated) => [
+            ...currentParticipated,
+            {
+              lastParticipant,
+              lastNumber,
+              lastRandom,
+              winner,
+            },
+          ]);
+          setLoading(false);
+        }
+      );
+    }
+  }, [smartContract]);
 
   return (
-    <div>
+    <div className="m-4 d-flex justify-content-center">
       {account ? (
-        <div>
-          <h1>Connected Account</h1>
-          <p>{account}</p>
-          {/* App Code goes here */}
+        <div className="d-flex flex-column align-items-center">
+          <Alert key="success" variant="success" className="text-center m-6">
+            <h3>Connected Account</h3>
+            <p>{account}</p>
+          </Alert>
+
+          <Button
+            variant="outline-success"
+            disabled={loading}
+            onClick={!loading ? playGame : null}
+          >
+            {loading ? "Loading…" : smartContract ? "Play Again" : "Play Game"}
+          </Button>
+
+          {participated.length > 0 ? (
+            <div className="m-4">
+              {participated.map((p, i) => (
+                <div key={i}>
+                  <p>Participant: {p.lastParticipant}</p>
+                  {/* <p>Last Number: {p.lastNumber}</p>
+                  <p>Last Random: {p.lastRandom}</p> 
+                  <p>Winner: {p.winner}</p> */}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="m-4">
+              Start the Game by clicking on the "Play Game" Button
+            </div>
+          )}
         </div>
       ) : (
-        <button className='connect-button' onClick={connectWallet}>
+        <Button variant="success" onClick={connectWallet}>
           Connect to Moonbase Alpha
-        </button>
+        </Button>
       )}
-
     </div>
   );
 }
